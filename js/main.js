7b178bd6b837f6048e095d559b4057e12411255b
pHoeniX-svg <vansomecsam@gmail.com>
2021-10-03 16:58:39 +0100

    updated navigation scss,  index.html: added, back-to-top link, added font-awesome

--- js/main.js
+++ js/main.js
@@ -3,28 +3,27 @@
 // slice extracts a section of a string without modifying original string
 //offsetTop - A Number, representing the top position of the element, in pixels
 
-const currentYear = document.getElementById('date');
-
 const navbar = document.querySelector('.header__nav');
 const navToggle = document.querySelector('.nav-toggle');
-const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
-const backToTopLink = document.querySelector('.top-link');
+const linksContainer = document.querySelector('.links-container');
+// const backToTopLink = document.querySelector('.top-link');
 
 // select links
 const scrollLinks = document.querySelectorAll('.scroll-link');
-
-// ********** set date ************
-currentYear.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
 navToggle.addEventListener('click', () => {
   const containerHeight = linksContainer.getBoundingClientRect().height;
   const linksHeight = links.getBoundingClientRect().height;
 
-  containerHeight === 0
-    ? (linksContainer.style.height = `${linksHeight}px`)
-    : (linksContainer.style.height = 0);
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+    navToggle.innerHTML = `<i class="fas fa-times"></i>`;
+  } else {
+    linksContainer.style.height = 0;
+    navToggle.innerHTML = `<i class="fas fa-bars"></i>`;
+  }
 });
 
 // ********** fixed navbar ************
@@ -32,42 +31,11 @@ window.addEventListener('scroll', () => {
   const scrollHeight = window.scrollY;
   const navHeight = navbar.getBoundingClientRect().height;
 
-  scrollHeight > navHeight
-    ? navbar.classList.add('header__nav--fixed') &
-      backToTopLink.classList.add('show-link')
-    : navbar.classList.remove('header__nav--fixed') &
-      backToTopLink.classList.remove('show-link');
-  // scrollHeight > 500
-  //   ? backToTopLink.classList.add('show-link')
-  //   : backToTopLink.classList.remove('show-link');
-});
-
-// ********** smooth scroll ************
-scrollLinks.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const navHeight = navbar.getBoundingClientRect().height;
-    const containerHeight = linksContainer.getBoundingClientRect().height;
-    const fixedNav = navbar.classList.contains('header__nav--fixed');
-    const id = e.currentTarget.getAttribute('href').slice(1);
-    const element = document.getElementById(id);
-
-    let position = element.offsetTop - navHeight;
-
-    if (!fixedNav) {
-      position -= navHeight;
-    }
-    if (navHeight > 82) {
-      position += containerHeight;
-    }
-
-    window.scrollTo({
-      left: 0,
-      top: position,
-    });
-
-    // closes links on a smaller screen when scrolling
-    linksContainer.style.height = 0;
-  });
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('header__nav--fixed');
+    // backToTopLink.classList.add('show-link');
+  } else {
+    navbar.classList.remove('header__nav--fixed');
+    // backToTopLink.classList.remove('show-link');
+  }
 });
