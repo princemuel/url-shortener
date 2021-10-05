8d2c8ed8b0a2c65557932733f4e9f14434d0c83a
pHoeniX-svg <vansomecsam@gmail.com>
2021-10-05 01:00:05 +0100

    nav: responsive across devices, fixed on scroll , toggle on mobile

--- js/main.js
+++ js/main.js
@@ -1,38 +1,30 @@
-const getElement = (selector) => {
+function getElement(selector) {
   const element = document.querySelector(selector);
-  if (element) {
-    return element;
-  }
+
+  if (element) return element;
   throw Error(`Please check your selector, no element matches ${selector} `);
-};
+}
 
-const links = getElement('.nav-links');
-const navBtn = getElement('.nav-btn');
-
-navBtn.addEventListener('click', () => {
-  links.classList.toggle('show-links');
-});
-
-const navbar = document.querySelector('.header__nav');
-const navToggle = document.querySelector('.nav-toggle');
-const links = document.querySelector('.links');
-const linksContainer = document.querySelector('.links-container');
-// const backToTopLink = document.querySelector('.top-link');
-
-// select links
-const scrollLinks = document.querySelectorAll('.scroll-link');
+const navbar = getElement('.nav');
+const navBtn = getElement('.nav-toggle');
+const linksLeft = getElement('.nav-links--left');
+const linksRight = getElement('.nav-links--right');
+const linksContainer = getElement('.nav-links');
+const backToTopLink = getElement('.top-link');
 
 // ********** close links ************
-navToggle.addEventListener('click', () => {
+navBtn.addEventListener('click', () => {
   const containerHeight = linksContainer.getBoundingClientRect().height;
-  const linksHeight = links.getBoundingClientRect().height;
+  const linksHeight =
+    linksLeft.getBoundingClientRect().height +
+    linksRight.getBoundingClientRect().height;
 
   if (containerHeight === 0) {
     linksContainer.style.height = `${linksHeight}px`;
-    navToggle.innerHTML = `<i class="fas fa-times"></i>`;
+    navBtn.innerHTML = `<i class="fas fa-times"></i>`;
   } else {
     linksContainer.style.height = 0;
-    navToggle.innerHTML = `<i class="fas fa-bars"></i>`;
+    navBtn.innerHTML = `<i class="fas fa-bars"></i>`;
   }
 });
 
@@ -42,10 +34,10 @@ window.addEventListener('scroll', () => {
   const navHeight = navbar.getBoundingClientRect().height;
 
   if (scrollHeight > navHeight) {
-    navbar.classList.add('header__nav--fixed');
-    // backToTopLink.classList.add('show-link');
+    navbar.classList.add('nav--fixed');
+    backToTopLink.classList.add('show-link');
   } else {
-    navbar.classList.remove('header__nav--fixed');
-    // backToTopLink.classList.remove('show-link');
+    navbar.classList.remove('nav--fixed');
+    backToTopLink.classList.remove('show-link');
   }
 });
