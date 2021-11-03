7fe06495fe3c2ad5dfcdb7a0ea96015a9faf5d08
pHoeniX-svg <vansomecsam@gmail.com>
2021-11-03 22:11:36 +0100

    updated  js => main, shorten; removed the shorten.js script and defer on main.js in index.html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 import { shorten } from './shorten.js';
-
 export const BASE_URL = `https://api.shrtco.de/v2/`;
 
 const form = document.getElementById('form');
 const linkInput = document.getElementById('link-input');
 const submitBtn = document.getElementById('submit');
 
-linkInput.addEventListener('paste', (e) => {
-  let link = e.clipboardData.getData('text');
-  shorten(link);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let link = linkInput.value;
+  console.log(link);
+  console.log(shorten(link));
 });
+
+
+
+// linkInput.addEventListener('paste', (e) => {
+//   let link = e.clipboardData.getData('text');
+//   shorten(link); 
+// });
--- js/shorten.js
+++ js/shorten.js
@@ -1,18 +1,27 @@
-import { BASE_URL } from './main';
+import { BASE_URL } from './main.js';
 
 export const shorten = async (link) => {
   const url = `${BASE_URL}/shorten?url=${link}`;
+  try {
+    const response = await fetch(url);
 
-  const response = await fetch(url);
-  const data = await response.json();
-  const { ok, result } = data;
+    if (!response.ok) {
+      const msg = `There was an error "${response.status} ${response.statusText}"`;
+      throw new Error(msg);
+    }
 
-  //checks if the status = 200 then pulls out result from the data
-  if (ok) {
-    console.log(result);
-    return result;
-  } else {
-    //Need an alert component for this
-    console.error('There is an error with your request');
+    const data = await response.json();
+    console.log(data);
+    // the data object returns 2 values => ok and result
+    const { ok, result } = data;
+    if (ok) {
+      console.log(result);
+      return result;
+    } else {
+      //Need an alert component instead of this(for an invalid link)
+      console.error('There is an error with your request');
+    }
+  } catch (error) {
+    throw new Error(error);
   }
 };
