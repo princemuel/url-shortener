735c045afa653c66e2cbc30744228593aba16853
TechieNg <melissacinta@gmail.com>
2021-11-04 20:03:28 +0100

    removed form element and changed corresponding css

---
+++ js/display.js
@@ -0,0 +1,7 @@
+import { shorten } from './shorten.js';
+
+export const display =  () => {
+    const form = document.getElementById('form');
+    const linkInput = document.getElementById('link-input');
+    const submitBtn = document.getElementById('submit');
+};
