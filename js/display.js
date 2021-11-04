9ea379dfaf94d92b79a5bae47c7eeeaab7b1c545
TechieNg <melissacinta@gmail.com>
2021-11-04 22:35:54 +0100

    cleaned up code added comment

--- js/display.js
+++ js/display.js
@@ -2,29 +2,36 @@
 let links = (localStorage.getItem('shortenedLinks'))?JSON.parse(localStorage.getItem('shortenedLinks')):{
     shortenedLink: [],
 };
-
+// function to add my shortenedLinks to local storage
 function linksObjectUpdated() {
     localStorage.setItem('shortenedLinks', JSON.stringify(links));
 }
 
+// I imported this in the shorten.js beacuse I need the display function to be called only after the response has been gotten
 export const  addItems = (value)=> {
+    // call the display function
     display(value);
+    // empty out the input field
     document.getElementById('link-input').value ='';
-    
+    // create an object and assign it the values we want to store
     let linkDetails = {
         code: value.code,
         original_link: value.original_link,
         full_short_link: value.full_short_link
     }
+    // push the object unto our shortenedLink array
     links.shortenedLink.push(linkDetails);
 
+    // call the function to store the updated links in local storage
     linksObjectUpdated()
 };
 
+// Function to dispay the result in the dom, I think the eventlistener for the copy button should go here too
 const display =  (data) => {
-
+    // get the ul element
     let list = document.querySelector(".shortened-links")
 
+    // creates an li element and give it a class
     let item = document.createElement('li');
     item.classList.add("shortened-link")
 
@@ -52,20 +59,20 @@ const display =  (data) => {
     item.appendChild(leftDiv);
     item.appendChild(rightDiv);
 
+    // insert ontop of the previous link
     list.insertBefore(item, list.childNodes[0]);
 };
 
 
 function renderLinks() {
-    // if both the todo and completed list are empty return nothing close function
+    // if shortenedLink array is empty return nothing close function
     if (!links.shortenedLink.length) return;
 
     for (let i = 0; i < links.shortenedLink.length; i++) {
         let value = links.shortenedLink[i];
-        console.log(value.code)
         display(value);
     }
 }
 
-
+// call render links
 renderLinks();
