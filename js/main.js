28c9270dc815abc5d280f0cfb11cdd716cac8970
TechieNg <melissacinta@gmail.com>
2021-11-08 21:34:04 +0100

    checked for shortened links

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { getElement } from './get-element.js';
 import { shorten } from './shorten.js';
+import { links, display, linksObjectUpdated } from './display.js';
 export const BASE_URL = `https://api.shrtco.de/v2/`;
 
 const linkInput = getElement('#link-input', document);
@@ -7,7 +8,27 @@ const submitBtn = getElement('#submit', document);
 
 submitBtn.addEventListener('click', () => {
   let link = linkInput.value;
-  shorten(link);
+  
+  if (!links.shortenedLink.length) {
+    shorten(link);
+  } else {
+    const shortened = links.shortenedLink.find(linkDetails => linkDetails.original_link  === link)
+    if(shortened){
+      const index = links.shortenedLink.findIndex(linkDetails => linkDetails.original_link  === link)
+      links.shortenedLink = [
+        ...links.shortenedLink.slice(0, index),
+        ...links.shortenedLink.slice(index + 1)
+      ]
+      links.shortenedLink.push(shortened)
+      display(shortened)
+      console.log(links.shortenedLink)
+      linksObjectUpdated()
+    }else{
+      shorten(link)
+    }
+  }
+  
+  
 });
 
 // linkInput.addEventListener('paste', (e) => {
