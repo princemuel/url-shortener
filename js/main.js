8aeda364e184f127d63aebc8c9a2125cd70136a4
pHoeniX-svg <vansomecsam@gmail.com>
2021-11-11 01:34:14 +0100

    updated shorten and main

--- js/main.js
+++ js/main.js
@@ -1,34 +1,36 @@
+import { display, links, linksObjectUpdated } from './display.js';
 import { getElement } from './get-element.js';
 import { shorten } from './shorten.js';
-import { links, display, linksObjectUpdated } from './display.js';
-export const BASE_URL = `https://api.shrtco.de/v2/`;
+export const BASE_URL = `https://api.shrtco.de/v2`;
 
 const linkInput = getElement('#link-input', document);
 const submitBtn = getElement('#submit', document);
 
 submitBtn.addEventListener('click', () => {
   let link = linkInput.value;
-  
+
   if (!links.shortenedLink.length) {
     shorten(link);
   } else {
-    const shortened = links.shortenedLink.find(linkDetails => linkDetails.original_link  === link)
-    if(shortened){
-      const index = links.shortenedLink.findIndex(linkDetails => linkDetails.original_link  === link)
+    const shortened = links.shortenedLink.find(
+      (linkDetails) => linkDetails.original_link === link
+    );
+    if (shortened) {
+      const index = links.shortenedLink.findIndex(
+        (linkDetails) => linkDetails.original_link === link
+      );
       links.shortenedLink = [
         ...links.shortenedLink.slice(0, index),
-        ...links.shortenedLink.slice(index + 1)
-      ]
-      links.shortenedLink.push(shortened)
-      display(shortened)
-      console.log(links.shortenedLink)
-      linksObjectUpdated()
-    }else{
-      shorten(link)
+        ...links.shortenedLink.slice(index + 1),
+      ];
+      links.shortenedLink.push(shortened);
+      display(shortened);
+      console.log(links.shortenedLink);
+      linksObjectUpdated();
+    } else {
+      shorten(link);
     }
   }
-  
-  
 });
 
 // linkInput.addEventListener('paste', (e) => {
--- js/shorten.js
+++ js/shorten.js
@@ -1,26 +1,6 @@
 import { addItems } from './display.js';
 import { BASE_URL } from './main.js';
 
-export const _shorten = async (link) => {
-  const url = `${BASE_URL}/shorten?url=${link}`;
-
-  try {
-    const response = await fetch(url);
-    const clonedResponse = response.clone();
-
-    if (!clonedResponse.ok) {
-      const clonedData = await clonedResponse.json();
-      throw new Error(`code 0${clonedData.error_code} => ${clonedData.error}`);
-    }
-
-    const { result } = await response.json();
-    addItems(result);
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 export const shorten = async (link) => {
   const url = `${BASE_URL}/shorten?url=${link}`;
 
@@ -45,19 +25,41 @@ export const shorten = async (link) => {
     .catch((error) => console.log(error));
 };
 
-// fetch(url)
-// .then(response => {
-//   if (response.ok) return response.json();
-//   return response.json().then(response => {throw new Error(response.error)})
-// })
-// .then(data => {
-// //       // the data object returns 2 values => ok and result
-//       const { ok, result } = data;
-//       if (ok) {
-//         addItems(result);
-//         return result;
-//       } else {
-//         //Need an alert component instead of this(for an invalid link)
-//         console.error('There is an error with your request');
-//       }
-// }).catch(error => console.log(error))
+export const _shorten = async (link) => {
+  const url = `${BASE_URL}/shorten?url=${link}`;
+
+  try {
+    const response = await fetch(url);
+    const clonedResponse = response.clone();
+
+    if (!clonedResponse.ok) {
+      const clonedData = await clonedResponse?.json();
+      throw new Error(`code 0${clonedData.error_code} => ${clonedData.error}`);
+    }
+
+    const { result } = await response?.json();
+    addItems(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// const shortener = async (url) => {
+//   try {
+//     const data = await fetch(url);
+//     return [data, null];
+//   } catch (error) {
+//     console.log(error);
+//     return [null, error];
+//   }
+// };
+
+// export const pPpshorten = async (link) => {
+//   const url = `${BASE_URL}/shorten?url=${link}`;
+
+//   const [data, error] = await shortener(url);
+//   const [data1, error1] = await data;
+//   console.log(data1);
+//   // console.log(error);
+// };
