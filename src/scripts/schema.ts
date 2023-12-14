75e359b772056ce9d46b200f5f73195dd95bc478
Prince Muel <vansomecsam@gmail.com>
2023-12-14 04:08:57 +0100

    feat: update client response schema
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/scripts/schema.ts
+++ src/scripts/schema.ts
@@ -2,6 +2,16 @@ import { z } from 'zod';
 
 export const UrlSchema = z.string().url('invalid url').trim();
 
-export const ResponseSchema = z.object({
+export const ServerResponseSchema = z.object({
   short_url: UrlSchema,
 });
+
+export const ResponseSchema = z.discriminatedUnion('status', [
+  z.object({
+    status: z.literal('success'),
+    hash: z.string().min(8),
+    short_url: UrlSchema,
+    original_url: UrlSchema,
+  }),
+  z.object({ status: z.literal('failed'), error: z.string() }),
+]);
